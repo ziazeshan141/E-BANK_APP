848b148b00fcacaf5fd0f1c5d83be05c0df2d682
ziazeshan141@outlook.com <ziazeshan141@outlook.com>
2026-08-19 00:17:09 +0300

    Fix AppComponent unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,6 +20,7 @@ import { AppComponent } from './app.component';
 class MockHeaderComponent {}
 
 describe('AppComponent', () => {
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -36,20 +37,26 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'E-banka'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('E-banka');
-  });
-
-  it('should render title in a h1 tag', () => {
+  it('should render the app header', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
+
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent)
-      .toContain('Welcome to E-banka!');
+
+    expect(compiled.querySelector('app-header')).toBeTruthy();
   });
+
+  it('should render the router outlet', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
+  });
+
 });
