de2bfb20dee7359b5c923cf61b7b3e1832ef3c5b
ziazeshan141@outlook.com <ziazeshan141@outlook.com>
2026-08-19 00:08:46 +0300

    Fix AppComponent unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -9,8 +9,14 @@ import {
   TestBed, async
 } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Component } from '@angular/core';
 import { AppComponent } from './app.component';
-import { HeaderSideMenuComponent } from './header-side-menu/header-side-menu.component';
+
+@Component({
+  selector: 'app-header',
+  template: ''
+})
+class MockHeaderComponent {}
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -20,7 +26,7 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent,
-        HeaderSideMenuComponent
+        MockHeaderComponent
       ],
     }).compileComponents();
   }));
