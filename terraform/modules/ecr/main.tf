46903640e396de60f1c057bfb223e66f0381afae
ziazeshan141@outlook.com <ziazeshan141@outlook.com>
2026-08-20 16:56:35 +0300

    fixed syntax error

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -4,9 +4,10 @@ resource "aws_ecr_repository" "repo" {
 
   image_scanning_configuration {
     scan_on_push = true
+  }
 
-    encryption_configuration {
-      encryption_type = "AES256"
+  encryption_configuration {
+    encryption_type = "AES256"
   }
 
   tags = {
